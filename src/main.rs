c484912568a30f35484823d67f5be4d8afdc8dde
Sayan Mallick <snyxmk@gmail.com>
2023-09-10 23:23:06 +0530

    task 3

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,29 @@
-use serde_json;
+use serde_bencode::{from_str, value::Value};
 use std::env;
-// Available if you need it!
-// use serde_bencode
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "5"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else if encoded_value.chars().next().unwrap() == 'i' {
-        let e_index = encoded_value.find('e').unwrap();
-        let number_string = &encoded_value[1..e_index];
-        let number = number_string.parse::<i64>().unwrap();
 
-        return serde_json::Value::Number(serde_json::Number::from(number));
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
-    }
+fn format(v: &Value) -> String {
+    return match v {
+        Value::Bytes(b) => format!("{:?}", String::from_utf8(b.clone()).unwrap()),
+        Value::Int(i) => i.to_string(),
+        Value::List(l) => format!(
+            "[{}]",
+            l.iter()
+                .map(|v| format(v))
+                .collect::<Vec<String>>()
+                .join(", ")
+        ),
+        _ => panic!("unsupported type"),
+    };
 }
-// Usage: your_bittorrent.sh decode "<encoded_value>"
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
+
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        // println!("Logs from your program will appear here!");
-        // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        let decoded_value = from_str::<Value>(encoded_value).unwrap();
+        println!("{}", format(&decoded_value));
     } else {
         println!("unknown command: {}", args[1])
     }
