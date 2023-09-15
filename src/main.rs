e0e855e9b75a472dabc55607033f5253ff6f49c5
Sayan Mallick <snyxmk@gmail.com>
2023-09-15 19:41:33 +0530

    stage 8 - test 1

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use serde_bencode::{from_bytes, from_str, to_bytes, value::Value};
 use serde_bytes::ByteBuf;
 use sha1::{Digest, Sha1};
 use std::env;
+use std::fmt::Display;
 use std::io::Read;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -31,6 +32,33 @@ impl<'a> std::fmt::LowerHex for HexSlice<'a> {
             write!(f, "{:02x}", byte)?;
         }
         Ok(())
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TrackerResponse {
+    interval: i64,
+    peers: ByteBuf,
+}
+impl Display for TrackerResponse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.get_peers().join("\n"))
+    }
+}
+impl TrackerResponse {
+    fn get_peers(&self) -> Vec<String> {
+        self.peers
+            .chunks(6)
+            .map(|chunk| {
+                let ip = chunk[0..4]
+                    .iter()
+                    .map(|b| b.to_string())
+                    .collect::<Vec<String>>()
+                    .join(".");
+                let port = u16::from_be_bytes([chunk[4], chunk[5]]);
+                format!("{}:{}", ip, port)
+            })
+            .collect()
     }
 }
 
@@ -87,6 +115,44 @@ fn main() {
         println!("Info Hash: {:x}", hash);
         println!("Piece Length: {}", decoded.info.piece_length);
         println!("Piece Hashes:\n{}", pieces_hashes.join("\n"));
+    } else if command == "peers" {
+        let mut file = std::fs::File::open(&args[2]).unwrap();
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer).unwrap();
+        let decoded: MetaInfo = from_bytes(&buffer).unwrap();
+
+        let hash = format!("{:x}", Sha1::digest(to_bytes(&decoded.info).unwrap()));
+
+        let hash =
+            hash.chars()
+                .collect::<Vec<char>>()
+                .chunks(2)
+                .fold(String::new(), |acc, chuck| {
+                    let symbol = chuck.iter().collect::<String>();
+                    let c = u8::from_str_radix(&symbol, 16).unwrap();
+                    match c {
+                        45 | 46 | 48..=57 | 65..=90 | 95 | 97..=122 => {
+                            acc + &char::from(c).to_string()
+                        }
+                        _ => acc + "%" + &symbol,
+                    }
+                });
+
+        let peer_id = "00112233445566778899";
+        let port = 6881;
+        let uploaded = 0;
+        let downloaded = 0;
+        let left = decoded.info.length;
+        let compact = 1;
+
+        let url = format!(
+            "{}?info_hash={}&peer_id={}&port={}&uploaded={}&downloaded={}&left={}&compact={}",
+            decoded.announce, hash, peer_id, port, uploaded, downloaded, left, compact
+        );
+
+        let response = reqwest::blocking::get(&url).unwrap().bytes().unwrap();
+        let decoded: TrackerResponse = from_bytes(&response).unwrap();
+        println!("{}", decoded);
     } else {
         println!("unknown command: {}", args[1])
     }
